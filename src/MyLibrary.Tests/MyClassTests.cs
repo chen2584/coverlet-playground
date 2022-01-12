506b291692c4c27b6658251f3d5b4a76e6c22532
Worameth Semapat <worameth@ookbee.com>
2022-01-12 17:57:30 +0700

    Update - Cover All Test

--- src/MyLibrary.Tests/MyClassTests.cs
+++ src/MyLibrary.Tests/MyClassTests.cs
@@ -4,18 +4,23 @@ namespace MyLibrary.Tests
 {
     public class MyClassTest
     {
-        readonly IMyClass _myClass;
-        public MyClassTest()
-        {
-            _myClass = new MyClass();
-        }
-
         [Fact]
         public void Sum_Of_Two_Values()
         {
             var myClass = new MyClass();
             var result = myClass.Sum(1, 1);
             Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Verify_If_It_Is_Even_Number()
+        {
+            var myClass = new MyClass();
+            var result = myClass.IsEven(2);
+            Assert.True(result);
+            
+            // result = myClass.IsEven(1);
+            // Assert.True(result);
         }
     }
 }
